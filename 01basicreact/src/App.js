287e1583165aabd5c9490004c9e715f7ad4a9e07
shivam singh <shivam.forapp@gmail.com>
2024-01-02 19:20:56 +0530

    react dom explained

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,11 +1,15 @@
-
+import Chai from './Chai'
 
 function App() {
   return (
-<h1>
-  Chai aur React | Shivam
-</h1>
+    <>
+      <h1>
+        Chai aur React | Shivam
+      </h1>
+      <Chai/>
+    </>
   );
 }
 
 export default App;
+ 
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,18 @@
+import Chai from './Chai'
+
 
 function App() {
 
 
   return (
+
+    <>
       <h1> 
         Chai aur React with Vite | Shivam
-         </h1>
+      </h1>
+      <Chai/>
+
+    </>
   )
 }
 
