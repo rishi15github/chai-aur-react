eeb507a81568dfe193bfba92de36760ca8ec34e9
shivam <shivam.forapp@gmail.com>
2024-01-04 18:55:34 +0530

    props in react

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -10,12 +10,27 @@ function App() {
     age: 21
   }
   let newArr = [1, 2, 3]
+  const num = 3
+
+  let indents = []
+  for (let index = 0; index < 3; index++) {
+    indents.push(
+      <Card username="hitesh" />
+    );
+    
+  }
+  
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
       <Card username="chaiaurcode" btnText="click me" />
       <Card username="hitesh" />
+
+      {indents}
+        
+
+
     </>
   )
 }
